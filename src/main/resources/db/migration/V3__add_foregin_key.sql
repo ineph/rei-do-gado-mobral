7d1697dc4a11582a4b744b0a7560b1ac8b37647b
Lucas Del Vecchio <lcs999999999@hotmail.com>
2023-10-13 10:11:16 -0300

    Atualizando gitignore conforme arquivos criados em build e dockerização

--- src/main/resources/db/migration/V3__add_foregin_key.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE public.fazendas ADD cd_id_usuario int8 NOT NULL;
-
-ALTER TABLE public.fazendas
-    ADD CONSTRAINT fk_fazenda_usuario_id FOREIGN KEY (cd_id_usuario) REFERENCES usuarios (id);
